9fea893757c002fdbc5038ae16e3c9dbef480b68
Ashley Sullins & teresa fletcher <student@epicodus.com>
2015-10-07 15:39:44 -0700

    Add inital practice files

---
+++ app.js
@@ -0,0 +1,1 @@
+var eventDetail = angular.module('eventDetail', []);
---
+++ js/EventController.js
@@ -0,0 +1,10 @@
+eventDetail.controller('EventCtrl', function EventCtrl($scope) {
+   $scope.details = [
+    { name: "Getting Groovy with Angular",
+      location: "New Relic"},
+    { name: "Let's get loopy with loops",
+      location: "Treehouse"},
+    { name: "Find an awesome Jr. Dev Job",
+      location: "CrowdCompass"}
+  ]
+});
